e694da1215477deb9ccbf1e57c1852de4496d782
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-09-19 15:02:53 +0900

    Define and update ui components #41
    Resolve #41

--- apps/main/src/app/page.tsx
+++ apps/main/src/app/page.tsx
@@ -1,16 +1,16 @@
 // @ts-expect-error - MDX file import not recognized by TypeScript
 import Welcome from './welcome.mdx';
-import { Header } from 'ui';
+import { Header, Prose } from 'ui';
 
 export default function Index() {
   return (
     <div>
       <Header />
       <main>
-        <div className="w-full max-w-3xl mx-auto py-10 px-4">
-          <article className="prose dark:prose-invert prose-zinc-light  prose-headings:font-semibold prose-h1:text-3xl">
+        <div className="w-full max-w-3xl mx-auto py-12 px-4">
+          <Prose>
             <Welcome />
-          </article>
+          </Prose>
         </div>
       </main>
     </div>
---
+++ packages/ui/src/lib/prose/prose.tsx
@@ -0,0 +1,7 @@
+export function Prose({ children }: { children: React.ReactNode }) {
+  return (
+    <article className="prose dark:prose-invert prose-zinc-light prose-headings:font-semibold prose-h1:text-3xl prose-a:underline-offset-[3px] prose-a:decoration-auto hover:prose-a:bg-zinc-200 dark:hover:prose-a:bg-zinc-700  prose-a:decoration-zinc-700 dark:prose-a:decoration-zinc-300">
+      {children}
+    </article>
+  );
+}
