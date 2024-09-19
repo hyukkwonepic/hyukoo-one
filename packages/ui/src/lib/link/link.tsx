e092fe8c34587c66e5af2f8e5765026503ec9c4d
Hyukoo Kwon <hyuk.kwon.epic@outlook.com>
2024-09-19 15:49:44 +0900

    Implement minor updates across projects #45 (#46)
    Resolve #45

--- packages/ui/src/lib/link/link.tsx
+++ packages/ui/src/lib/link/link.tsx
@@ -16,6 +16,7 @@ export function Link({ children, ...props }: Props) {
 export function linkStyles() {
   return clsx(
     'inline-flex items-center tracking-tight font-medium leading-6 text-zinc-700 hover:bg-zinc-200 ',
-    'dark:text-zinc-300 dark:hover:bg-zinc-700'
+    'dark:text-zinc-300 dark:hover:bg-zinc-700',
+    'underline decoration-auto underline-offset-[5px] decoration-zinc-700 dark:decoration-zinc-300'
   );
 }
